f089ded4b4fa8e55932b8969179b1f32012bbe2f
Jonathan Pujol <jonathan.pujol12@gmail.com>
2021-01-28 11:40:37 +0100

    Passing test to order tea without sugar

--- src/main/java/coffee/DrinkMakerProtocol.java
+++ src/main/java/coffee/DrinkMakerProtocol.java
@@ -3,6 +3,9 @@ package coffee;
 public class DrinkMakerProtocol {
 
     public String format(Order order) {
+        if(Drink.TEA == order.getBeverage()) {
+            return "T::";
+        }
         return "C::";
     }
 }
