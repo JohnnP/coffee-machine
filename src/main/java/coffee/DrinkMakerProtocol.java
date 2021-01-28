82d518fb40846600d658984e3807526eba91f8ef
Jonathan Pujol <jonathan.pujol12@gmail.com>
2021-01-28 11:50:29 +0100

    Refacto: Removing duplication code

--- src/main/java/coffee/DrinkMakerProtocol.java
+++ src/main/java/coffee/DrinkMakerProtocol.java
@@ -5,12 +5,6 @@ import static coffee.Drink.*;
 public class DrinkMakerProtocol {
 
     public String format(Order order) {
-        if(CHOCOLATE == order.getBeverage()) {
-            return order.getBeverage().getCode() + "::";
-        }
-        if(TEA == order.getBeverage()) {
-            return order.getBeverage().getCode() + "::";
-        }
         return order.getBeverage().getCode() + "::";
     }
 }
