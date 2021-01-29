985e2c7ace8d524b82ee19c1b5a248d82b2e20d4
Jonathan Pujol <jonathan.pujol12@gmail.com>
2021-01-29 11:48:46 +0100

    Refacto: Add more intention

--- src/main/java/coffee/DrinkMakerProtocol.java
+++ src/main/java/coffee/DrinkMakerProtocol.java
@@ -2,8 +2,21 @@ package coffee;
 
 public class DrinkMakerProtocol {
 
+    public static final int WITHOUT_SUGAR = 0;
+
     public String format(Order order) {
-        return order.getDrinkCode() + (order.getSugar() != 0 ? ":" + order.getSugar() +":0" : "::");
+        return getDrinkCode(order) + manageSugar(order);
+    }
+
+    private String getDrinkCode(Order order) {
+        return order.getDrinkCode();
+    }
+
+    private String manageSugar(Order order) {
+        if (WITHOUT_SUGAR == order.getSugar()) {
+            return "::";
+        }
+        return ":" + order.getSugar() + ":0";
     }
 
 }
--- src/test/java/coffee/DrinkMakerProtocolTest.java
+++ src/test/java/coffee/DrinkMakerProtocolTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import static coffee.Drink.*;
+import static coffee.DrinkMakerProtocol.WITHOUT_SUGAR;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DrinkMakerProtocolTest {
 
-    public static final int WITHOUT_SUGAR = 0;
     private static final int WITH_ONE_SUGAR = 1;
     private static final int WITH_TWO_SUGAR = 2;
 
@@ -23,33 +23,33 @@ class DrinkMakerProtocolTest {
     }
 
     @Test
-    void should_order_coffee_without_sugar() {
+    void should_order_a_coffee() {
         when_order(COFFEE, WITHOUT_SUGAR);
         formatter_should_return("C::");
     }
 
     @Test
-    void should_order_coffee_with_one_sugar() {
-        when_order(COFFEE, WITH_ONE_SUGAR);
-        formatter_should_return("C:1:0");
-    }
-
-    @Test
-    void should_order_coffee_with_two_sugar() {
-        when_order(COFFEE, WITH_TWO_SUGAR);
-        formatter_should_return("C:2:0");
-    }
-
-    @Test
-    void should_order_tea_without_sugar() {
+    void should_order_a_tea() {
         when_order(TEA, WITHOUT_SUGAR);
        formatter_should_return("T::");
     }
 
     @Test
-    void should_order_chocolate_without_sugar() {
+    void should_order_a_chocolate() {
         when_order(CHOCOLATE, WITHOUT_SUGAR);
         formatter_should_return("H::");
+    }
+
+    @Test
+    void should_order_drink_with_one_sugar() {
+        when_order(COFFEE, WITH_ONE_SUGAR);
+        formatter_should_return("C:1:0");
+    }
+
+    @Test
+    void should_order_drink_with_two_sugar() {
+        when_order(COFFEE, WITH_TWO_SUGAR);
+        formatter_should_return("C:2:0");
     }
 
     void when_order(Drink drink, int sugar) {
