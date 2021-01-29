2023e145433f8ffaf4b9c6854c19b131e4e85a08
Jonathan Pujol <jonathan.pujol12@gmail.com>
2021-01-29 11:19:19 +0100

    Passing test to order coffee with one sugar

--- src/main/java/coffee/DrinkMakerProtocol.java
+++ src/main/java/coffee/DrinkMakerProtocol.java
@@ -3,7 +3,7 @@ package coffee;
 public class DrinkMakerProtocol {
 
     public String format(Order order) {
-        return order.getDrinkCode() + "::";
+        return order.getDrinkCode() + (order.getSugar() == 1 ? ":1:0" : "::");
     }
 
 }
